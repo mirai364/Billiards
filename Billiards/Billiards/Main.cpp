c141ad4fa27b7336428bc952894a1bd720db2121
mirai364 <mirai364@gmail.com>
2016-10-31 17:44:56 +0900

    ビリヤード台を表示(Xファイル読み込み)

--- Billiards/Billiards/Main.cpp
+++ Billiards/Billiards/Main.cpp
@@ -1,232 +1,260 @@
-#define     szClassName TEXT("3D Box Rotate")
-
+#include <stdio.h>
 #include <windows.h>
+#include <d3d9.h>
 #include <d3dx9.h>
-#include <dxerr.h>
-#include <tchar.h>
 
 #pragma once
 #pragma comment(lib,"winmm.lib")
 
-//頂点フォーマット
-
-#define D3DFVF_VERTEX  (D3DFVF_XYZ | D3DFVF_DIFFUSE)	//トランスフォームされていない頂点の位置座標+ディフューズ カラー成分
-
-typedef struct _D3DVERTEXCOR_ {
-	float   x, y, z;	// トランスフォームされていない頂点の位置座標
-	DWORD   color;	// ディフューズ カラー成分
-}   D3DVERTEXCOR;
-
-
-#define TIME_SET 500.0f	//	立方体を回転させるスピード 1/1000秒単位
-
-HWND                    hWnd;
-LPDIRECT3DDEVICE9       g_pDEV = 0;
-LPDIRECT3D9             pD3D;
-D3DPRESENT_PARAMETERS   D3DPPWin;
+//-----------------------------------------------------------------
+//    Grobal Variables.
+//-----------------------------------------------------------------
+LPDIRECT3D9             g_pD3D = NULL;
+LPDIRECT3DDEVICE9       g_pd3dDevice = NULL;
+LPD3DXMESH              g_pMesh = NULL;
+LPD3DXBUFFER			g_pMaterial;
+DWORD					dwNumMaterials = NULL;
+D3DMATERIAL9*			g_pMeshMaterials = NULL;
+LPDIRECT3DTEXTURE9*		g_pMeshTextures = NULL;
+D3DXMATERIAL*			d3dxMaterials;
 
 
-//	立方体の頂点座標を定義（座標は立方体の中心を原点とする)
+
+//-----------------------------------------------------------------
+//    Prototypes.
+//-----------------------------------------------------------------
+HWND    InitApp(HINSTANCE, int);
+BOOL    InitDirect3D(HWND);
+BOOL    CleanupDirect3D();
+BOOL    SetupMatrices();
+BOOL    RenderDirect3D();
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 
-D3DVERTEXCOR v1[] =     //前面赤の矩形 
-{ { -50, -50,  50, 0xffff0000 },
-{ 50, -50,  50, 0xffff0000 },
-{ -50,  50,  50, 0xffff0000 },
-{ 50,  50,  50, 0xffff0000 } };
-D3DVERTEXCOR v2[] =     //右側面緑の矩形
-{ { 50, -50,  50, 0xff00ff00 },
-{ 50, -50, -50, 0xff00ff00 },
-{ 50,  50,  50, 0xff00ff00 },
-{ 50,  50, -50, 0xff00ff00 } };
-D3DVERTEXCOR v3[] =     //上面青の矩形
-{ { -50, -50, -50, 0xff0000ff },
-{ 50, -50, -50, 0xff0000ff },
-{ -50, -50,  50, 0xff0000ff },
-{ 50, -50,  50, 0xff0000ff } };
-D3DVERTEXCOR v4[] =     //左側面黄の矩形
-{ { -50,  50,  50, 0xffffff00 },
-{ -50,  50, -50, 0xffffff00 },
-{ -50, -50,  50, 0xffffff00 },
-{ -50, -50, -50, 0xffffff00 } };
-D3DVERTEXCOR v5[] =     //裏面紫の矩形 
-{ { -50,  50, -50, 0xffff00ff },
-{ 50,  50, -50, 0xffff00ff },
-{ -50, -50, -50, 0xffff00ff },
-{ 50, -50, -50, 0xffff00ff } };
-D3DVERTEXCOR v6[] =     //下面白の矩形
-{ { 50,  50, -50, 0xffffffff },
-{ -50,  50, -50, 0xffffffff },
-{ 50,  50,  50, 0xffffffff },
-{ -50,  50,  50, 0xffffffff } };
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-HRESULT Init3DDev(HWND hwnd);
-void  SetupMatrices(float x, float y, float z = 0);
-void Draw(void);
+//-----------------------------------------------------------------
+//    Main.
+//-----------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevinst, LPSTR nCmdLine, int nCmdShow)
+{
+	MSG msg;
+	HWND hWnd;
 
+	ZeroMemory(&msg, sizeof(msg));  //msg初期化
+	ZeroMemory(&g_pMaterial, sizeof(g_pMaterial));  //material初期化
 
-int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int) {
-	MSG     msg;
+	hWnd = InitApp(hInst, nCmdShow);
+	if (!hWnd) return FALSE;
 
-	WNDCLASSEX wc = { sizeof(WNDCLASSEX),CS_CLASSDC,WndProc,0L,0L,hInst,
-		NULL,NULL,NULL,NULL,szClassName,NULL };
-	if (RegisterClassEx(&wc) == 0)
-		return FALSE;
-	HWND  hWnd = CreateWindowEx(0, szClassName, szClassName, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 600, 600,
-		NULL, NULL, hInst, NULL);
-	if (FAILED(Init3DDev(hWnd)))	return FALSE;
+	if (!InitDirect3D(hWnd)) return FALSE;
 
-	ShowWindow(hWnd, SW_SHOWDEFAULT);
-	UpdateWindow(hWnd);
-	ZeroMemory(&msg, sizeof(msg));
 	while (msg.message != WM_QUIT) {
-		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE)) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		else
-			Draw();
+		else {
+			RenderDirect3D();
+		}
+		Sleep(1);
 	}
-	g_pDEV->Release();
-	pD3D->Release();
-	UnregisterClass(szClassName, wc.hInstance);
-	return 0;
+
+	return msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+
+//-----------------------------------------------------------------
+//    Initialize Application.
+//-----------------------------------------------------------------
+HWND InitApp(HINSTANCE hInst, int nCmdShow)
+{
+	WNDCLASS wc;
+	HWND hWnd;
+	char szClassName[] = "Direct3D Test";
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hInstance = hInst;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszClassName = szClassName;
+	wc.lpszMenuName = NULL;
+	wc.lpfnWndProc = WndProc;
+	wc.cbWndExtra = 0;
+	wc.cbClsExtra = 0;
+	if (!RegisterClass(&wc)) return FALSE;
+
+	hWnd = CreateWindow(szClassName, "Direct3D Test", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+		NULL, NULL, hInst, NULL);
+	if (!hWnd) return FALSE;
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return hWnd;
+}
+
+
+
+//-----------------------------------------------------------------
+//    Initialize Direct3D.
+//-----------------------------------------------------------------
+BOOL InitDirect3D(HWND hWnd)
+{
+	D3DPRESENT_PARAMETERS d3dpp;
+	HRESULT hr;
+
+	if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION))) {
+		MessageBox(hWnd, "Can't create Direct3D.", "Error", MB_OK);
+		return FALSE;
+	}
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.EnableAutoDepthStencil = TRUE;
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+
+	hr = g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		&d3dpp, &g_pd3dDevice);
+	if (FAILED(hr)) {
+		MessageBox(hWnd, "Can't create device.", "Error", MB_OK);
+		return FALSE;
+	}
+
+	g_pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
+
+	g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
+	
+	// Load model data
+	D3DXLoadMeshFromX(".\\table_9.x", D3DXMESH_SYSTEMMEM, g_pd3dDevice,
+		NULL, &g_pMaterial, NULL, &dwNumMaterials, &g_pMesh);
+
+	// Material load
+	d3dxMaterials = (D3DXMATERIAL*)g_pMaterial -> GetBufferPointer();
+	g_pMeshMaterials = new D3DMATERIAL9[dwNumMaterials];
+	g_pMeshTextures = new LPDIRECT3DTEXTURE9[dwNumMaterials];
+
+	for (DWORD i = 0; i<dwNumMaterials; i++)
+	{
+			g_pMeshMaterials[i] = d3dxMaterials[i].MatD3D;
+			g_pMeshMaterials[i].Ambient = g_pMeshMaterials[i].Diffuse;
+			g_pMeshTextures[i] = NULL;
+
+			if (d3dxMaterials[i].pTextureFilename != NULL && lstrlen(d3dxMaterials[i].pTextureFilename) > 0) {
+				D3DXCreateTextureFromFile(g_pd3dDevice, d3dxMaterials[i].pTextureFilename, &g_pMeshTextures[i]);
+			}
+	}
+
+
+	return TRUE;
+}
+
+
+
+//-----------------------------------------------------------------
+//    Cleanup Direct3D.
+//-----------------------------------------------------------------
+BOOL CleanupDirect3D()
+{
+	if (g_pMesh != NULL)
+		g_pMesh->Release();
+
+	if (g_pd3dDevice != NULL)
+		g_pd3dDevice->Release();
+
+	if (g_pD3D != NULL)
+		g_pD3D->Release();
+
+	return TRUE;
+}
+
+
+
+//-----------------------------------------------------------------
+//    Window Proc.
+//-----------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
 	switch (msg) {
 	case WM_DESTROY:
+		CleanupDirect3D();
 		PostQuitMessage(0);
-		return 0L;
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wp, lp);
 	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
+
+	return 0L;
 }
 
-//デバイス/モード等の初期化
-HRESULT Init3DDev(HWND hwnd) {
 
-	hWnd = hwnd;
-	pD3D = NULL;
-	HRESULT     hr;
-	D3DDISPLAYMODE          dmode;
 
-	// デバイス/モード等の初期化
-	if (pD3D == NULL)
-		pD3D = Direct3DCreate9(D3D_SDK_VERSION);
-	if (pD3D == NULL)
-		return E_FAIL;
-	//現在のディスプレイモードを得る
-	if (FAILED(pD3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &dmode)))
-		return E_FAIL;
+//-----------------------------------------------------------------
+//    Setup Matrices.
+//-----------------------------------------------------------------
+BOOL SetupMatrices()
+{
+	D3DXMATRIXA16 matWorld, matView, matProj;
+	D3DXVECTOR3 vEyePt, vLookatPt, vUpVec;
 
-	// D3DDeviceオブジェクトの作成
-	ZeroMemory(&D3DPPWin, sizeof(D3DPPWin));
-	D3DPPWin.BackBufferFormat = dmode.Format;
-	D3DPPWin.BackBufferCount = 1;
-	D3DPPWin.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	D3DPPWin.Windowed = TRUE;
-	D3DPPWin.EnableAutoDepthStencil = TRUE;
-	D3DPPWin.AutoDepthStencilFormat = D3DFMT_D16;
-	D3DPPWin.Flags = D3DPRESENTFLAG_LOCKABLE_BACKBUFFER;
+	// World Matrix.
+	D3DXMatrixRotationY(&matWorld, timeGetTime() / 1000.0f);
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
 
-	hr = pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
-		D3DCREATE_HARDWARE_VERTEXPROCESSING, &D3DPPWin, &g_pDEV);
-	if (FAILED(hr)) {
-		hr = pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
-			D3DCREATE_SOFTWARE_VERTEXPROCESSING, &D3DPPWin, &g_pDEV);
-		if (FAILED(hr)) {
-			hr = pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd,
-				D3DCREATE_SOFTWARE_VERTEXPROCESSING, &D3DPPWin, &g_pDEV);
-			if (FAILED(hr)) {
-				MessageBox(NULL, TEXT("Create Device Error"), TEXT("Surface Error"), MB_OK);
-				return E_FAIL;
-			}
-		}
+	// Camera.
+	vEyePt.x = 0.0f;
+	vEyePt.y = 3.0f;
+	vEyePt.z = 0.0f - 5.0f;
+	vLookatPt.x = 0.0f;
+	vLookatPt.y = 0.0f;
+	vLookatPt.z = 0.0f;
+	vUpVec.x = 0.0f;
+	vUpVec.y = 1.0f;
+	vUpVec.z = 0.0f;
+	D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);
+	g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
+
+	// Projection Matrix.
+	D3DXMatrixPerspectiveFovLH(&matProj, 3.0f / 4.0f, 1.0f, 1.0f, 100.0f);
+	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
+
+	return TRUE;
+}
+
+
+
+
+//-----------------------------------------------------------------
+//    Render Direct3D.
+//-----------------------------------------------------------------
+BOOL RenderDirect3D()
+{
+	RECT rc;
+	D3DXVECTOR3 center, position;
+	
+
+	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
+		0x00000000, 1.0f, 0);
+
+	g_pd3dDevice->BeginScene();
+
+	SetupMatrices();
+
+	for (DWORD i = 0; i < dwNumMaterials; i++)
+	{
+		g_pd3dDevice->SetMaterial(&g_pMeshMaterials[i]);
+		g_pd3dDevice->SetTexture(0, g_pMeshTextures[i]);
+		g_pMesh->DrawSubset(i);
 	}
-	return S_OK;
-}
 
+	g_pd3dDevice->EndScene();
 
+	g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 
-//描画環境の設定
-void  SetupMatrices(float x, float y, float z) {
-	D3DXMATRIX      matView;
-	D3DXMATRIX      matProj;
-	D3DXMATRIX      w;
-
-	D3DXVECTOR3 v;
-	v.x = -x;
-	v.y = -z;
-	v.z = -y;
-
-	D3DXMatrixRotationAxis(&w, &v, timeGetTime() / TIME_SET); // 任意の軸を回転軸にして回転する行列を作成する。 システム時刻をミリ秒単位で取得
-
-
-	D3DXMatrixTranslation(&matView, x, y, z); // オフセットを指定して行列を作成する。
-
-	matView = w*matView;
-	g_pDEV->SetTransform(D3DTS_VIEW, &matView);
-
-	//透視変換の設定
-	D3DXMatrixPerspectiveFovLH(&matProj, D3DXToRadian(45.0f), 1.0f, -300, 300); // 視野に基づいて、左手座標系パースペクティブ射影行列を作成する
-	g_pDEV->SetTransform(D3DTS_PROJECTION, &matProj);
-	//描画設定
-	g_pDEV->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);		//反時計回りの面を消去
-	g_pDEV->SetRenderState(D3DRS_LIGHTING, FALSE);         	//ライティングしない
-	g_pDEV->SetRenderState(D3DRS_ZENABLE, D3DZB_FALSE);    	//Zバッファ使わない
-}
-
-//描画処理
-void Draw(void) {
-	if (!g_pDEV)
-		return;
-	//黒で塗りつぶして消去
-	g_pDEV->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 1.0, 0);
-	//描画の開始
-	g_pDEV->BeginScene();
-
-	//描画環境の設定
-
-	SetupMatrices(80, 80, 400);
-	//デバイスに使用する頂点フォーマットをセットする
-	g_pDEV->SetFVF(D3DFVF_VERTEX);
-	//ストリームを使わないで直接データを渡して描画する
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v1, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v2, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v3, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v4, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v5, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v6, sizeof(D3DVERTEXCOR));
-
-	SetupMatrices(80, -80, 400);
-	//ストリームを使わないで直接データを渡して描画する
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v1, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v2, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v3, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v4, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v5, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v6, sizeof(D3DVERTEXCOR));
-
-	SetupMatrices(-80, 80, 400);
-	//ストリームを使わないで直接データを渡して描画する
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v1, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v2, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v3, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v4, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v5, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v6, sizeof(D3DVERTEXCOR));
-
-	SetupMatrices(-80, -80, 400);
-	//ストリームを使わないで直接データを渡して描画する
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v1, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v2, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v3, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v4, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v5, sizeof(D3DVERTEXCOR));
-	g_pDEV->DrawPrimitiveUP(D3DPT_TRIANGLESTRIP, 2, v6, sizeof(D3DVERTEXCOR));
-
-	g_pDEV->EndScene();
-	g_pDEV->Present(NULL, NULL, NULL, NULL);
+	return TRUE;
 }
